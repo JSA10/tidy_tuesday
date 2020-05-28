57f9347c235a6cab09f740441ae2ceef872b91cc
JSA10 <jerome.ahye@gmail.com>
2020-05-28 22:00:31 +0100

    Upload final chart and related code in separate file to exploratory work

--- eda_tidytuesday_2020-05-26.R
+++ eda_tidytuesday_2020-05-26.R
@@ -1,5 +1,8 @@
+
+# mainly relying on the tidyverse package for this one
 library(tidyverse)
 
+# *** TL:DR - skip to end for code to build the final chart ***
 
 
 # Get the data ------------------------------------------------------------
@@ -7,23 +10,11 @@ library(tidyverse)
 cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
 boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
 
-# Or read in with tidytuesdayR package (https://github.com/thebioengineer/tidytuesdayR)
-
-# Either ISO-8601 date or year/week works!
-
-# Install via devtools::install_github("thebioengineer/tidytuesdayR")
-
-# tuesdata <- tidytuesdayR::tt_load('2020-05-26')
-# tuesdata <- tidytuesdayR::tt_load(2020, week = 22)
+# Mr Boston dataset supposed to be cleaner than the cocktails one which was web scraped so
+# has some 'funkiness' but does contains extra features that may be of value
 
 
-#cocktails <- tuesdata$cocktails
-
-# Mr Boston dataset supposed to be cleaner and the cocktails one was web scraped so
-# has some 'funkiness' but also contains extra features that may be of value
-
-
-# Explore Mr Boston dataset -----------------------------------------------
+# KISS - Explore Mr Boston dataset -----------------------------------------------
 
 dplyr::glimpse(boston_cocktails)
 # the measure column has been left as a string with number and volume/unit to clean up
@@ -31,11 +22,11 @@ dplyr::glimpse(boston_cocktails)
 head(boston_cocktails)
 # data lists ingredients and their measures (rows),
 # within cocktails (name = categorical variable) that can
-# be grouped by cocktail type / genre (category = categorical variable).
+# be grouped by cocktail genre (category = categorical variable).
 
 # two index variables have been added to the data set, identifying number of
 # ingredients within each cocktail (ingredient_number) and cocktails themselves (row_id)
-# - could be value in adding an index variable for each ingredient, so can see
+# ... could be value in adding an index variable for each ingredient, so can see
 # how they feature across cocktails
 
 summary(boston_cocktails)
@@ -47,10 +38,11 @@ boston_cocktails %>%
 
 
 
-# What use cases could a viewer of this data set have? --------------------
+# Think about audience - what would be useful to a viewer of my visualisation? --------------------
 
 # rstats enthusaist - looking for examples of good use of r's data prep and
-# plotting features and hints and tricks for their work
+# plotting features or hints and tricks for their work
+# Looking for:
 # - clarity
 # - insight
 # - aesthetics
@@ -61,7 +53,7 @@ boston_cocktails %>%
 # - what do I need to make my favourite cocktail?
 # - what cocktails can I make with the drinks I have?
 
-# hiring managers
+# industry / managers
 # - logical thinking
 # - thinking about end user need and clarity of insight
 # - technical skill
@@ -92,15 +84,25 @@ dplyr::glimpse(cocktails)
 
 head(cocktails, 10) %>%
     View()
+# might be bigger range of drinks but otherwise not loads of obvious value for
+# my user needs to get into the type of glass etc.
+#
+# also, want to deliver something useful, quickly so going to stay clear of
+# wrangling with the measures column for now, looks very time consuming.
 
-
-# Visuals based on simplest user needs  -------------------------------------
+# Exploratory visuals based on simplest user needs  -------------------------------------
 
 # Q - what are the most flexible ingredients to keep in your cupboards?
 
 boston_cocktails %>%
-    count(ingredient, sort = TRUE)
+    count(ingredient, sort = TRUE) %>%
+    View()
 # 569 ingredients - gin, vodka and light rum and most common hard spirits
+# possibly irregular caseusage in text
+
+boston_cocktails %>%
+    count(ingredient, sort = TRUE)
+# reduced number of ingredients from 569 to 554
 
 boston_cocktails %>%
     count(ingredient, sort = TRUE) %>%
@@ -122,4 +124,160 @@ boston_cocktails %>%
     theme_minimal()
 # need to find way to make facets only feature the top 5 - 10 ingredients in each category and for
 # x axis to remove unnecessary ingredients
+
+
+# Top ingredients by category ---------------------------------------------
+
+top_5_by_cat <- boston_cocktails %>%
+    group_by(category) %>%
+    count(ingredient, sort = TRUE) %>%
+    top_n(5)
+
+png("top_5_ingredients_category.png", width = 1200, height = 900)
+top_5_by_cat %>%
+    filter(n != 1) %>%
+    ggplot2::ggplot(aes(x = reorder(ingredient, -n), y = n)) +
+    geom_col(aes(fill = category)) +
+    facet_wrap(category ~ ., scales = "free_x") +
+    geom_text(aes(label = n, vjust = -0.5)) +
+    labs(x = "Top Ingredients", y = "Number of recipes featured in",
+         title = "Which cocktail ingredients should you stock up on?",
+         subtitle = "The most flexible cocktail ingredients for each type") +
+    scale_x_discrete(guide = guide_axis(n.dodge=2)) +
+    theme_minimal() +
+    theme(
+        axis.text.x = element_text(size = rel(1.2)),
+        axis.text.y = element_text(size = rel(1.5)),
+        axis.title.y = element_text(size = rel(2)),
+        axis.title.x = element_text(size = rel(2)),
+        strip.text = element_text(size = rel(1.5)),
+        plot.title =  element_text(size = rel(2.5)),
+        plot.subtitle = element_text(size = rel(1.5))
+    )
+dev.off()
+
+
+
+# Jazz up plot aesthetics -------------------------------------------------
+
+# somehow cocktails make me think of the grand budapest hotel image, so going to
+# try out this wesanderson colour pallette package
+install.packages("wesanderson")
+library(wesanderson)
+names(wes_palettes)
+
+grand_budapest_pink_9 <- wesanderson::wes_palette("GrandBudapest2", 9,
+                                                type = "continuous")
+
+png("top_5_ingredients_category_palette.png", width = 1200, height = 900)
+top_5_by_cat %>%
+    filter(n != 1) %>%
+    ggplot2::ggplot(aes(x = reorder(ingredient, -n), y = n)) +
+    geom_col(aes(fill = category)) +
+    facet_wrap(category ~ ., scales = "free_x") +
+    geom_text(aes(label = n, vjust = -0.5)) +
+    labs(x = "Top Ingredients", y = "Number of recipes featured in",
+         title = "Which cocktail ingredients should you stock up on?",
+         subtitle = "The most flexible cocktail ingredients for each type") +
+    scale_x_discrete(guide = guide_axis(n.dodge=2)) +
+    theme_minimal(base_family = "Archer book") +
+    theme(
+        axis.text.x = element_text(size = rel(1.2)),
+        axis.text.y = element_text(size = rel(1.5)),
+        axis.title.y = element_text(size = rel(2)),
+        axis.title.x = element_text(size = rel(2)),
+        strip.text = element_text(size = rel(1.5)),
+        plot.title =  element_text(size = rel(2.5)),
+        plot.subtitle = element_text(size = rel(1.5))
+    ) +
+    scale_fill_manual(values = grand_budapest_pink_9)
+dev.off()
+# could make this more impactful by cutting some categories
+
+# note - Archer font looks to be the one used in grand budapest, could download
+# font and add to that visual
+
+
+
+# Refine data for plot -------------------------------------------------------------
+
+# could just cut down the number of ingredients visualised
+top_6_by_cat <- boston_cocktails %>%
+    group_by(category) %>%
+    count(ingredient, sort = TRUE) %>%
+    top_n(6)
+
+# looks like worth grouping some of the commonly used items together e.g. juices
+# - taking 'top down' approach here, could be more exhaustive, but time.
+boston_cocktails_cleaner <- boston_cocktails %>%
+    mutate_if(is.character, tolower) %>%
+    mutate(ingredient = stringr::str_replace_all(ingredient,
+                                                 c("juice of a lemon" = "lemon juice",
+                                                   "juice of a lime" = "lime juice",
+                                                   "juice of a orange" = "orange juice",
+                                                   "fresh orange juice" = "orange juice",
+                                                   "fresh lime juice" = "lime juice",
+                                                   "fresh lemon juice" = "lemon juice",
+                                                   "simple syrup" = "sugar syrup")))
+
+# don't need spectrum of colours, can stick to original colour pallette of 4 colours
+# if cutting down categories
+
+grand_budapest_pink <- wesanderson::wes_palette("GrandBudapest2", 4)
+
+# group and summarise the data
+boston_ingredients_by_category <- boston_cocktails_cleaner %>%
+    group_by(category) %>%
+    count(ingredient, sort = TRUE)
+
+# stumbled upon a known problem with ordering within facets.
+# found solution via https://github.com/tidyverse/ggplot2/issues/1902
+boston_ordered <- boston_ingredients_by_category %>%
+    top_n(10) %>%
+    ungroup() %>%
+    arrange(category, desc(n)) %>%
+    mutate(order = row_number()) %>%
+    filter(n != 1) %>%
+    filter(category %in% c("cocktail classics", "tequila",
+                           "vodka", "whiskies"))
+# As a self respecting Trinidadian; can't conscience the promotion of rum for use in daiquiris!
+
+# solution for facet ordering continues below, with new order var used as x axis and labels added
+# within scale_x_continuous
+# - sounds like more efficient / reliable solution is to use a custom function :
+# https://github.com/dgrtwo/drlib/blob/master/R/reorder_within.R
+
+
+# Presentation chart --------------------------------------------------------
+
+png("top_5_ingredients_top_categories_palette.png", width = 1200, height = 900)
+boston_ordered %>%
+    ggplot2::ggplot(aes(x = order, y = n)) +
+    geom_col(aes(fill = category)) +
+    facet_wrap(category ~ ., scales = "free_x") +
+    geom_text(aes(label = n, vjust = -0.5)) +
+    scale_x_continuous(breaks = boston_ordered$order,     # notice need to reuse data frame
+                     labels = boston_ordered$ingredient,
+                     guide = guide_axis(n.dodge=3)) +
+    scale_fill_manual(values = grand_budapest_pink) +
+    labs(x = "Top Ingredients for each type", y = "Number of recipes featured in",
+         title = "Lockdown cocktail ingredients to stock up on...",
+         subtitle = "The most flexible cocktail ingredients for your tipple of choice",
+         caption = "By Jerome Ahye. Source: Recipes from Mr Boston cocktail guide, data from Kaggle
+         via https://github.com/rfordatascience/tidytuesday") +
+    theme_minimal(base_family = "Archer") +
+    theme(
+        axis.text.x = element_text(size = rel(1.2)),
+        axis.text.y = element_text(size = rel(1.5)),
+        axis.title.y = element_text(size = rel(2)),
+        axis.title.x = element_text(size = rel(2)),
+        strip.text = element_text(size = rel(1.5)),
+        plot.title =  element_text(size = rel(2.5)),
+        plot.subtitle = element_text(size = rel(1.5)),
+        legend.position = "none"
+    )
+dev.off()
+
+
+
 
---
+++ pres_tidytuesday_2020-05-26.R
@@ -0,0 +1,104 @@
+
+# mainly relying on the tidyverse package for this one
+library(tidyverse)
+# I've referenced packages used in code below, but will need this for the
+# pipe operators - %>%
+
+# *** TL:DR - skip to end for code to build the final chart ***
+
+
+# Get the data ------------------------------------------------------------
+
+#cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
+
+# Mr Boston dataset supposed to be cleaner than the cocktails one which was web scraped so
+# has some 'funkiness' but does contains extra features that may be of value
+
+
+# Refine data for plot -------------------------------------------------------------
+
+# looks like worth grouping some of the commonly used items together e.g. juices
+# - taking 'top down' approach here, could be more exhaustive, but time.
+boston_cocktails_cleaner <- boston_cocktails %>%
+    mutate_if(is.character, tolower) %>%
+    mutate(ingredient = stringr::str_replace_all(ingredient,
+                                                 c("juice of a lemon" = "lemon juice",
+                                                   "juice of a lime" = "lime juice",
+                                                   "juice of a orange" = "orange juice",
+                                                   "fresh orange juice" = "orange juice",
+                                                   "fresh lime juice" = "lime juice",
+                                                   "fresh lemon juice" = "lemon juice",
+                                                   "simple syrup" = "sugar syrup")))
+
+
+
+# Jazz up plot aesthetics -------------------------------------------------
+
+# somehow cocktails make me think of the grand budapest hotel image, so going to
+# try out this wesanderson colour pallette package
+
+# gone for pink version and can stick to original 4 colour pallette
+# as cutting down categories to 4
+grand_budapest_pink <- wesanderson::wes_palette("GrandBudapest2", 4)
+
+
+# group and order data within facets ------------------------------------------------
+
+# group and summarise the data
+boston_ingredients_by_category <- boston_cocktails_cleaner %>%
+    dplyr::group_by(category) %>%
+    dplyr::count(ingredient, sort = TRUE)
+
+# stumbled upon a known problem with ordering within facets.
+# found solution via https://github.com/tidyverse/ggplot2/issues/1902
+boston_ordered <- boston_ingredients_by_category %>%
+    dplyr::top_n(10) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(category, desc(n)) %>%
+    dplyr::mutate(order = row_number()) %>%
+    dplyr::filter(n != 1) %>%
+    dplyr::filter(category %in% c("cocktail classics", "tequila",
+                           "vodka", "whiskies"))
+# As a self respecting Trinidadian; can't conscience the promotion of rum for use in daiquiris!
+
+
+# solution for facet ordering continues below, with new order var used as x axis and labels added
+# within scale_x_continuous
+
+# - sounds like more efficient / reliable solution is to use a custom function :
+# https://github.com/dgrtwo/drlib/blob/master/R/reorder_within.R
+
+
+# Presentation chart --------------------------------------------------------
+
+png("lockdown_ingredients_final.png", width = 1200, height = 900)
+boston_ordered %>%
+    ggplot2::ggplot(aes(x = order, y = n)) +
+    geom_col(aes(fill = category)) +
+    facet_wrap(category ~ ., scales = "free_x") +
+    geom_text(aes(label = n, vjust = -0.5)) +
+    scale_x_continuous(breaks = boston_ordered$order,     # notice need to reuse data frame
+                     labels = boston_ordered$ingredient,
+                     guide = guide_axis(n.dodge=3)) +
+    scale_fill_manual(values = grand_budapest_pink) +
+    labs(x = "Top Ingredients for each type", y = "Number of recipes featured in",
+         title = "Lockdown cocktail ingredients to stock up on...",
+         subtitle = "The most flexible cocktail ingredients for your tipple of choice",
+         caption = "By Jerome Ahye. Source: Recipes from Mr Boston cocktail guide, data from Kaggle
+         via https://github.com/rfordatascience/tidytuesday") +
+    theme_minimal(base_family = "Rockwell") +
+    theme(
+        axis.text.x = element_text(size = rel(1.2)),
+        axis.text.y = element_text(size = rel(1.5)),
+        axis.title.y = element_text(size = rel(2)),
+        axis.title.x = element_text(size = rel(2)),
+        strip.text = element_text(size = rel(1.5)),
+        plot.title =  element_text(size = rel(2.5)),
+        plot.subtitle = element_text(size = rel(1.5)),
+        legend.position = "none"
+    )
+dev.off()
+
+
+
